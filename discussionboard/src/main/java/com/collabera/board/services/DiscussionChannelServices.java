1363d393284cf58c20575da0d4f71409dec0f430
Robert-Kirk <d.foxultra@gmail.com>
2019-09-24 00:31:18 -0500

    eliminating dead code

--- discussionboard/src/main/java/com/collabera/board/services/DiscussionChannelServices.java
+++ discussionboard/src/main/java/com/collabera/board/services/DiscussionChannelServices.java
@@ -3,7 +3,6 @@ package com.collabera.board.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.stereotype.Service;
 
 import com.collabera.board.models.DiscussionChannel;
@@ -23,7 +22,7 @@ public class DiscussionChannelServices{
 	
 	
 	public List<DiscussionChannel> getList(String title) {
-		TextCriteria criteria = TextCriteria.forDefaultLanguage().matching(title);
+		
 		return repo.findAllByTitle();
 	}
 
